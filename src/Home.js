3f6215ff5430912a2d2be1beb63d573b7906dd8c
Aanandi Sharma <84490483+Aanandi05@users.noreply.github.com>
2025-10-19 00:42:03 +0530

    Update Home.js

--- src/Home.js
+++ src/Home.js
@@ -1,54 +1,72 @@
 import React, { Component } from 'react';
 import { Input, Button, IconButton } from '@material-ui/core';
 import GitHubIcon from '@material-ui/icons/GitHub';
-import "./Home.css"
+import "./Home.css";
 
 class Home extends Component {
-  	constructor (props) {
-		super(props)
-		this.state = {
-			url: ''
-		}
-	}
+  constructor(props) {
+    super(props);
+    this.state = {
+      url: ''
+    };
+  }
 
-	handleChange = (e) => this.setState({ url: e.target.value })
+  handleChange = (e) => this.setState({ url: e.target.value });
 
-	join = () => {
-		if (this.state.url !== "") {
-			var url = this.state.url.split("/")
-			window.location.href = `/${url[url.length-1]}`
-		} else {
-			var url = Math.random().toString(36).substring(2, 7)
-			window.location.href = `/${url}`
-		}
-	}
+  join = () => {
+    if (this.state.url !== "") {
+      const urlParts = this.state.url.split("/");
+      window.location.href = `/${urlParts[urlParts.length - 1]}`;
+    } else {
+      const url = Math.random().toString(36).substring(2, 7);
+      window.location.href = `/${url}`;
+    }
+  }
 
-	render() {
-		return (
-			<div className="container2">
-				<div style={{fontSize: "14px", background: "white", width: "10%", textAlign: "center", margin: "auto", marginBottom: "10px"}}>
-					Source code: 
-					<IconButton style={{color: "black"}} onClick={() => window.location.href="https://github.com/Aanandi05/Video-Calling-Application"}>
-						<GitHubIcon />
-					</IconButton>
-				</div>
-				
-				<div>
-					<h1 style={{ fontSize: "45px" }}>Video Meeting</h1>
-					<p style={{ fontWeight: "200" }}>Video conference website that lets you stay in touch with all your friends.</p>
-				</div>
+  render() {
+    return (
+      <div className="home-wrapper">
+        {/* Top decorative panel with GitHub */}
+        <div className="top-panel">
+          <h2 className="top-title">Video Meeting</h2>
+          <IconButton
+            onClick={() => window.open("https://github.com/Aanandi05/Video-Calling-Application", "_blank")}
+            className="github-btn"
+          >
+            <GitHubIcon fontSize="large" />
+          </IconButton>
+        </div>
 
-				<div style={{
-					background: "white", width: "30%", height: "auto", padding: "20px", minWidth: "400px",
-					textAlign: "center", margin: "auto", marginTop: "100px"
-				}}>
-					<p style={{ margin: 0, fontWeight: "bold", paddingRight: "50px" }}>Start or join a meeting</p>
-					<Input placeholder="URL" onChange={e => this.handleChange(e)} />
-					<Button variant="contained" color="primary" onClick={this.join} style={{ margin: "20px" }}>Go</Button>
-				</div>
-			</div>
-		)
-	}
+        {/* Main hero section */}
+        <section className="hero-section">
+          <div className="hero-card">
+            <h1 className="hero-title">Connect with Everyone</h1>
+            <p className="hero-subtitle">
+              Video conference website that lets you stay in touch with all your friends.
+            </p>
+
+            {/* Join/Create meeting */}
+            <div className="meeting-panel">
+              <p className="panel-title">Start or join a meeting</p>
+              <Input
+                placeholder="Enter URL"
+                value={this.state.url}
+                onChange={this.handleChange}
+                className="meeting-input"
+              />
+              <Button
+                variant="contained"
+                className="meeting-btn"
+                onClick={this.join}
+              >
+                Go
+              </Button>
+            </div>
+          </div>
+        </section>
+      </div>
+    );
+  }
 }
 
 export default Home;
